a7b0b26988ce5b59443e2e73b80b3fe42623cd06
full-stack-david <davidchan12323@gmail.com>
2023-08-30 16:55:32 -0500

    Add full Keyboard

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <template>
-  <h1>Vue Simple Keyboard</h1>
+  <div class="container">
+    <textarea :value="input" @input="onInputChange" @keyup="onPress">
+    </textarea>
+    <SimpleKeyboard @onChange="onChange" @onKeyPress="onKeyPress" :status="status" :input="input"/>
+  </div>
 </template>
 
 <script>
+import SimpleKeyboard from "./SimpleKeyboard";
+import "./App.css";
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    SimpleKeyboard
+  },
+  data: () => ({
+    input: "",
+    status: ""
+  }),
+  methods: {
+    onChange(input) {
+      this.input = input;
+    },
+    onKeyPress(button) {
+      console.log("button", button);
+    },
+    onPress(e) {
+      this.status = e.key
+    },
+    onInputChange(input) {
+      this.input = input.target.value;
+    }
+  }
 };
 </script>
---
+++ src/SimpleKeyboard.vue
@@ -0,0 +1,152 @@
+<template>
+  <div class="keyboardContainer">
+    <div class="simple-keyboard-main"></div>
+
+    <div class="controlArrows">
+      <div class="simple-keyboard-control"></div>
+      <div class="simple-keyboard-arrows"></div>
+    </div>
+
+    <div class="numPad">
+      <div class="simple-keyboard-numpad"></div>
+      <div class="simple-keyboard-numpadEnd"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Keyboard from "simple-keyboard";
+import "simple-keyboard/build/css/index.css";
+export default {
+  name: "SimpleKeyboard",
+  props: {
+    input: String,
+    status: String
+  },
+  data: () => ({
+    keyboard: null,
+    keyboardControlPad: null,
+    keyboardArrows: null,
+    keyboardNumPad: null,
+    keyboardNumPadEnd: null
+  }),
+  mounted() {
+    let commonKeyboardOptions = {
+      onChange: input => this.onChange(input),
+      onKeyPress: button => this.onKeyPress(button),
+      theme: "simple-keyboard hg-theme-default hg-layout-default",
+      physicalKeyboardHighlight: true,
+      syncInstanceInputs: true,
+      mergeDisplay: true,
+    };
+    this.keyboard = new Keyboard(".simple-keyboard-main", {
+      ...commonKeyboardOptions,
+      layout: {
+        default: [
+          "{escape} {f1} {f2} {f3} {f4} {f5} {f6} {f7} {f8} {f9} {f10} {f11} {f12}",
+          "` 1 2 3 4 5 6 7 8 9 0 - = {backspace}",
+          "{tab} q w e r t y u i o p [ ] \\",
+          "{capslock} a s d f g h j k l ; ' {enter}",
+          "{shiftleft} z x c v b n m , . / {shiftright}",
+          "{controlleft} {altleft} {metaleft} {space} {metaright} {altright}"
+        ],
+        shift: [
+          "{escape} {f1} {f2} {f3} {f4} {f5} {f6} {f7} {f8} {f9} {f10} {f11} {f12}",
+          "~ ! @ # $ % ^ & * ( ) _ + {backspace}",
+          "{tab} Q W E R T Y U I O P { } |",
+          '{capslock} A S D F G H J K L : " {enter}',
+          "{shiftleft} Z X C V B N M < > ? {shiftright}",
+          "{controlleft} {altleft} {metaleft} {space} {metaright} {altright}"
+        ]
+      },
+      display: {
+        "{escape}": "Esc",
+        "{tab}": "Tab ⇥",
+        "{backspace}": "Backspace ⌫",
+        "{enter}": "Enter ↵",
+        "{capslock}": "Caps Lock ⇪",
+        "{shiftleft}": "Shift ⇧",
+        "{shiftright}": "Shift ⇧",
+        "{controlleft}": "Ctrl ⌃",
+        "{controlright}": "Ctrl ⌃",
+        "{altleft}": "Alt ⌥",
+        "{altright}": "Alt ⌥",
+        "{metaleft}": "cmd ⌘",
+        "{metaright}": "cmd ⌘"
+      }
+    });
+    this.keyboardControlPad = new Keyboard(".simple-keyboard-control", {
+      ...commonKeyboardOptions,
+      layout: {
+        default: [
+          "{prtscr} {scrolllock} {pause}",
+          "{insert} {home} {pageup}",
+          "{delete} {end} {pagedown}"
+        ]
+      }
+    });
+    this.keyboardArrows = new Keyboard(".simple-keyboard-arrows", {
+      ...commonKeyboardOptions,
+      layout: {
+        default: ["{arrowup}", "{arrowleft} {arrowdown} {arrowright}"]
+      }
+    });
+    this.keyboardNumPad = new Keyboard(".simple-keyboard-numpad", {
+      ...commonKeyboardOptions,
+      layout: {
+        default: [
+          "{numlock} {numpaddivide} {numpadmultiply}",
+          "{numpad7} {numpad8} {numpad9}",
+          "{numpad4} {numpad5} {numpad6}",
+          "{numpad1} {numpad2} {numpad3}",
+          "{numpad0} {numpaddecimal}"
+        ]
+      }
+    });
+    this.keyboardNumPadEnd = new Keyboard(".simple-keyboard-numpadEnd", {
+      ...commonKeyboardOptions,
+      layout: {
+        default: ["{numpadsubtract}", "{numpadadd}", "{numpadenter}"]
+      }
+    });
+  },
+  methods: {
+    onChange(input) {
+      this.$emit("onChange", input);
+    },
+    onKeyPress(button) {
+      this.$emit("onKeyPress", button);
+      
+      if (
+        button === "{shift}" ||
+        button === "{shiftleft}" ||
+        button === "{shiftright}" ||
+        button === "{capslock}"
+      ) {
+        this.handleShift();
+      }
+    },
+    handleShift() {
+      let currentLayout = this.keyboard.options.layoutName;
+      let shiftToggle = currentLayout === "default" ? "shift" : "default";
+      this.keyboard.setOptions({
+        layoutName: shiftToggle
+      });
+    }
+  },
+  watch: {
+    input(input) {
+      this.keyboard.setInput(input);
+    },
+    status(status) {
+      if (status == 'Shift' || status == 'CapsLock') {  
+        let currentLayout = this.keyboard.options.layoutName;
+        let shiftToggle = currentLayout === "default" ? "shift" : "default";
+        this.keyboard.setOptions({
+          layoutName: shiftToggle
+        });
+      }
+    }
+  }
+};
+</script>
